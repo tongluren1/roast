7fa3279da171b191acfe3565f360ce00d2950a0c
guozheng <2084759131@qq.com>
2019-02-25 22:15:36 +0800

    2019-2-25-第一节

---
+++ app/Http/Controllers/Web/AppController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 20847
+ * Date: 2019/2/25
+ * Time: 22:06
+ */
+
+namespace app\Http\COntrollers\Web;
+
+use App\Http\Controllers\Controller;
+
+class AppController extends Controller
+{
+    public function getApp()
+    {
+        return view('app');
+    }
+}
